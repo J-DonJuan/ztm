2efaf17ac591a5736b2a62816a32e8d708429112
J-DonJuan <jcdonnelly48@gmail.com>
2023-03-15 17:07:48 -0700

    Finished first part of dom-events, added exercise txt for reinforcement exercise later

--- javascript/dom-events/dom-events.js
+++ javascript/dom-events/dom-events.js
@@ -3,8 +3,19 @@ var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 
 button.addEventListener("click", function(){
+    if (input.value.length > 0) {
     var li = document.createElement("li");
-    li.appendChild(document.createTextNode("testing"));
+    li.appendChild(document.createTextNode(input.value));
     ul.appendChild(li);
-})
+    input.value = "";
+}})
 
+input.addEventListener("keypress", function(event){
+    if (input.value.length > 0 && event.code === "Enter") {
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
+}})
+
+console.log(input);
