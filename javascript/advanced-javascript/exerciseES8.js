991fa42fc465baf9ab0491112554c812728d9d5e
J-DonJuan <jcdonnelly48@gmail.com>
2023-03-30 22:42:36 -0700

    Finished ExerciseES8

---
+++ javascript/advanced-javascript/exerciseES8.js
@@ -0,0 +1,40 @@
+// Solve the below problems:
+
+// #1) Line up the Turtle and the Rabbit at the start line:
+const startLine = '     ||<- Start line';
+let turtle = '🐢';
+let rabbit = '🐇';
+
+// it should look like this:
+'     ||<- Start line'
+'       🐢'
+'       🐇'
+
+turtle = turtle.padStart(11, ' ');
+rabbit = rabbit.padStart(11, ' ');
+
+// when you do:
+console.log(startLine);
+console.log(turtle);
+console.log(rabbit);
+
+
+// #2) What happens when you run turtle.trim().padEnd(9, '=') on the turtle variable
+// Read about what the second parameter does in padEnd and padStart
+turtle = turtle.trim().padEnd(9, '=');
+
+
+// #3) Get the below object to go from:
+let obj = {
+  my: 'name',
+  is: 'Rudolf',
+  the: 'raindeer'
+}
+let newStr = '';
+Object.entries(obj).forEach(array => {
+  newStr = newStr.concat(array[0] + ' ' + array[1] + ' ');
+})
+console.log(newStr);
+
+// to this:
+// 'my name is Rudolf the raindeer'
--- javascript/advanced-javascript/script.js
+++ javascript/advanced-javascript/script.js
@@ -3,3 +3,16 @@ let obj = {
     name1: 'Rudolph',
     name2: 'Grinch'
 }
+
+const objKeys = Object.keys(obj);
+console.log(objKeys);
+objKeys.forEach((key) => {
+    console.log(obj[key]);
+})
+
+const objValues = Object.values(obj);
+console.log(objValues);
+
+const objEntries = Object.entries(obj);
+console.log(objEntries);
+console.log(objEntries[0][0])
