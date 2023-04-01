c2279f95ac0b72f8cde9938397955d3c916e34c6
J-DonJuan <jcdonnelly48@gmail.com>
2023-04-01 16:49:40 -0700

    Finished advanced looping exercise, started ES10-Exercise

--- javascript/advanced-javascript/script.js
+++ javascript/advanced-javascript/script.js
@@ -1,18 +1,16 @@
-let obj = {
-    name0: 'Santa',
-    name1: 'Rudolph',
-    name2: 'Grinch'
-}
 
-const objKeys = Object.keys(obj);
-console.log(objKeys);
-objKeys.forEach((key) => {
-    console.log(obj[key]);
-})
+let array = [1,2,3,[4,5]];
 
-const objValues = Object.values(obj);
-console.log(objValues);
 
-const objEntries = Object.entries(obj);
-console.log(objEntries);
-console.log(objEntries[0][0])
+let a = '   Hello';
+console.log(a.trimStart());
+
+
+
+// // Sample Array
+// let obj = {
+//     name0: 'Santa',
+//     name1: 'Rudolph',
+//     name2: 'Grinch'
+// }
+
